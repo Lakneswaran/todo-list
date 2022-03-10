7f093e9d4fefee1da71eca05edf3b736ec167c17
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2022-03-10 12:10:21 +0100

    component listitem to remove a item or done

---
+++ src/todo/ListItem.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+export default function ListItem(props){
+    return(
+        <>
+           <div> 
+            <p>{props.name}</p>
+            <button>Mark as done</button>
+            <button>Delete</button>
+           </div>
+        </>
+    );
+}
