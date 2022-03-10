28bd205075adf786b10d016925e3bfcf9ae6340b
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2022-03-10 12:18:12 +0100

    inputfield created with to getter and setter using useState and fuc for changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [todoName, setTodoName] = useState("");
+  const updateToName = (e) => {
+    setTodoName(e.target.value);
+  }
   return (
    <>
       <div>
         <h3 className="border-blue-500 border bg-blue-300 hover:bg-blue-800">Todo list</h3>
+        <input
+          type="text"
+          placeholder='Add your todo value'
+          value={todoName}
+          onChange={updateToName}
+        />
       </div>
    </>
   );
