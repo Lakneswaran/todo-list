bf4512768a09ee32fcbd803ec2dc20653cb46194
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2022-03-10 12:00:47 +0100

    tailwind applied

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
    <>
+      <div>
+        <h3 className="border-blue-500 border bg-blue-300 hover:bg-blue-800">Todo list</h3>
+      </div>
    </>
   );
 }
